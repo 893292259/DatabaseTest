8cca1ec637af51e36a52266755c93d970ea0230a
zzliu <liuzhizheng@sjtu.edu.cn>
2018-02-28 20:30:27 +0800

    First Commit

---
+++ app/src/main/java/com/example/databasetest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.databasetest;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  MyDatabaseHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        helper = new MyDatabaseHelper(this,"BookStore.db",null,2);
+        Button createdb = findViewById(R.id.add_data);
+        createdb.setOnClickListener(v->{
+            SQLiteDatabase db = helper.getWritableDatabase();
+
+        });
+    }
+}
